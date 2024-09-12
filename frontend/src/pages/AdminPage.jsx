c96e1ff9de28e63f896fef618bfdfda9830ca30e
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-09-12 20:07:06 +0800

    Added the necessary fields

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function AdminPage() {
+  const [url, setUrl] = useState('')
+
+  const shortenLink = () => {
+
+  }
   return (
-    <div>AdminPage</div>
+    <div className='page-container'>
+        <div className="content-container">
+            <div className="input-container">
+                <div className="function">
+                    <input 
+                    type="text"
+                    value={url}
+                    onChange={(e) => setUrl(e.target.value)}
+                    />
+                    <button className="shorten-link" onSubmit={shortenLink}>Shrink</button>
+                </div>
+            </div>
+            <div className="data-container">
+
+            </div>
+        </div>
+    </div>
   )
 }
 
