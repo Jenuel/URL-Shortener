533e374e9a963aa8b72cf0df7f4dbce7180e62c0
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-09-15 19:07:29 +0800

    Set up the database

--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New() //Creates a new Instance
-	e.GET("/", func(c echo.Context) error {
+
+	dsn := "admin:admin@tcp(127.0.0.1:3306)/shortener"
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer db.Close()
+
+	//store
+	e.GET("/shrink", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 	e.Logger.Fatal(e.Start(":1323"))
