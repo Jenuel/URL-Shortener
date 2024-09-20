8198c2b46caab9f1654b63e9b1b07b3024c08e59
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-09-20 22:24:57 +0800

    Implemented the logic for the dataview

--- frontend/src/components/DataView.jsx
+++ frontend/src/components/DataView.jsx
@@ -1,9 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 function DataView() {
+
   return (
     <div>
-        DataView
-    </div>
+    <h1>Data View</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Original URL</th>
+          <th>Short Code</th>
+          <th>Clicks</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map(item => (
+          <tr key={item.id}>
+            <td><a href={item.original} target="_blank" rel="noopener noreferrer">{item.original}</a></td>
+            <td>{item.short}</td>
+            <td>{item.click}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
   )
 }
 
--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,16 +1,35 @@
 import React, { useState } from 'react'
+import DataView from '../components/DataView'
+import '../pages/AdminPage.css'
 
 function AdminPage() {
   const [url, setUrl] = useState('')
+  const [error, setError] = useState('')
+
 
   const shortenLink = (e) => {
     e.preventDefault()
     if(isUrlValid(url)){
-
+        shrinkUrl()
+        setError('')
     } else {
-        
+        //display an error message to the user
+        setError("Please enter a valid url")
     }
   }
+
+  const shrinkUrl = async () => {
+    try {
+      const response = await axios.post('http://localhost:1323/shrink', {
+        url: url, 
+      });
+  
+      setData(response.data); 
+    } catch (error) {
+      console.error('Error shortening URL:', error); 
+    }
+  };
+  
 
   function isUrlValid(input) {
     var res = input.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
@@ -39,7 +58,7 @@ function AdminPage() {
                 </div>
             </div>
             <div className="data-container">
-
+                <DataView/>
             </div>
         </div>
     </div>
