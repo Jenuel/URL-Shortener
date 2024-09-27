8fa3dba38c69618c5b9fed56a8a35d698b2e1e6b
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-09-27 16:12:09 +0800

    Wrap the AdminPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import AdminPage from './pages/AdminPage'
+import { UrlProvider } from './context/UrlContext';
 
 function App() {
  
 
   return (
-    <AdminPage/>
+    <UrlProvider>
+       <AdminPage/>
+    </UrlProvider>
   )
 }
 
